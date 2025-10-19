f9f3059375fd0f48f4ed26317c671d27a057ac86
vamshi <yenaganti.vamshi007@gmail.com>
2025-10-19 18:01:30 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const express=require('express');
+const app=express();
+app.use("/test",(req,res)=>{res.send("Hello from server")});
+app.listen(7777,()=>{console.log("server is running on port 7777")});
